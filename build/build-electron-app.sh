c00e47fc24c823a87a048aa72567b48f955d3327
Joseph Paul Cohen <joseph@josephpcohen.com>
2020-08-05 20:10:24 -0400

    update build script

--- build/build-electron-app.sh
+++ build/build-electron-app.sh
@@ -1,5 +1,5 @@
 ELECTRON_OSX_URL=https://github.com/electron/electron/releases/download/v9.1.2/electron-v9.1.2-darwin-x64.zip
-ELECTRON_WIN64_URL=https://github.com/electron/electron/releases/download/v9.1.2/electron-v9.1.2-linux-arm64.zip
+ELECTRON_WIN64_URL=https://github.com/electron/electron/releases/download/v9.1.2/electron-v9.1.2-win32-x64.zip
 
 mkdir -p ./cache
 #wget -c $ELECTRON_OSX_URL -O ./cache/$(basename $ELECTRON_OSX_URL)
@@ -20,14 +20,13 @@ cp ../system.js ./tmp/app/
 cp ../style.css ./tmp/app/
 cp -r ../res/ ./tmp/app/res/
 cp -r ../examples/ ./tmp/app/examples/
-cp -r ../models/ ./tmp/app/models/
 
 cp -r ../models/xrv-all-45rot15trans15scale ./tmp/app/models/ 
-cp -r ../models/chestxnet-45rot15trans15scale4byte ./tmp/app/models/ 
+cp -r ../models/ae-chest-savedmodel-64-512 ./tmp/app/models/ 
 
 
 #mac app
-unzip -q ./cache/$(basename $URL) -d ./tmp/
+unzip -q ./cache/$(basename $ELECTRON_OSX_URL) -d ./tmp/
 
 mv ./tmp/Electron.app ./tmp/Chester.app
 rm ./tmp/Chester.app/Contents/Resources/electron.icns
@@ -38,7 +37,8 @@ cp -r ./tmp/app/ ./tmp/Chester.app/Contents/Resources/app/
 cp app.js ./tmp/Chester.app/Contents/Resources/app/
 cp package.json ./tmp/Chester.app/Contents/Resources/app/
 
-#zip -r ./tmp/Chester.app.zip ./tmp/Chester.app
+#echo Compressing .app
+#zip -q -r ./tmp/Chester.app.zip ./tmp/Chester.app
 
 # windows app
 
